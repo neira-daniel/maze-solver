88b7f625a02dcbbc1ab480ce0f9070d2a4d1c450
Daniel Neira <keep.up.the.good.work@outlook.com>
2025-01-15 15:42:50 -0300

    docs: add comments to `graphics.py`

--- src/graphics.py
+++ src/graphics.py
@@ -2,7 +2,12 @@ from tkinter import Tk, BOTH, Canvas
 
 
 class Window:
+    """
+    Object that manages Tkinter windows on screen.
+    """
+
     def __init__(self, width: int, height: int) -> None:
+        """Initializes a Window object without displaying it on screen."""
         self.__root = Tk()
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__root.title("Maze Solver")
@@ -13,13 +18,17 @@ class Window:
         self.__running = False
 
     def redraw(self) -> None:
+        """Refreshes the Window object."""
         self.__root.update()
         self.__root.update_idletasks()
 
     def wait_for_close(self) -> None:
+        """Keeps the Window object alive and disposes of it gracefully when the user
+        presses the close button."""
         self.__running = True
         while self.__running:
             self.redraw()
 
     def close(self) -> None:
+        """Sends the closing signal to the Window object."""
         self.__running = False
