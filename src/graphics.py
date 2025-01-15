030d97609d29a79ec0b244496162fd50566e32ce
Daniel Neira <keep.up.the.good.work@outlook.com>
2025-01-15 17:37:56 -0300

    feat: add ability to draw lines

--- src/graphics.py
+++ src/graphics.py
@@ -32,3 +32,57 @@ class Window:
     def close(self) -> None:
         """Sends the closing signal to the Window object."""
         self.__running = False
+
+    def draw_line(self, line: "Line", fill_color: str = "black") -> None:
+        line.draw(canvas=self.__canvas, fill_color=fill_color)
+
+
+class Point:
+    """
+    Object that stores 2D-point coordinates in pixels.
+    """
+
+    def __init__(self, x: int, y: int) -> None:
+        """Initializes a Point object with `x` being the horizontal direction and `y` the vertical.
+
+        Note that the origin coordinate (x=0, y=0) represents the top left of the window."""
+        self.x = x
+        self.y = y
+
+    def __add__(self, other: "Point") -> "Point":
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: "Point") -> "Point":
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, factor: int | float) -> "Point":
+        """Handles Point object * factor"""
+        if isinstance(factor, (int, float)):
+            return Point(int(self.x * factor), int(self.y * factor))
+        return NotImplemented
+
+    def __rmul__(self, factor: int | float) -> "Point":
+        """Handles factor * Point object"""
+        return self.__mul__(factor)
+
+    def get_coordinates(self):
+        return self.x, self.y
+
+
+class Line:
+    """
+    Object that represents 2D lines.
+    """
+
+    def __init__(self, point_A: "Point", point_B: "Point"):
+        self.point_A = point_A
+        self.point_B = point_B
+
+    def draw(self, canvas: Canvas, fill_color: str = "black"):
+        """Draws a line between two Point objects.
+
+        Valid string colors for `fill_color` can be found here:
+        https://www.tcl.tk/man/tcl8.4/TkCmd/colors.htm"""
+        x1, y1 = self.point_A.get_coordinates()
+        x2, y2 = self.point_B.get_coordinates()
+        canvas.create_line(x1, y1, x2, y2, fill=fill_color, width=2)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 
 def main():
     win = Window(800, 600)
+    point_A = Point(100, 100)
+    point_B = Point(400, 400)
+    line_1 = Line(point_A, 0.5 * point_B)
+    line_2 = Line(point_A + point_B, point_B - point_A)
+    win.draw_line(line_1, "firebrick")
+    win.draw_line(line_2, "dark olive green")
     win.wait_for_close()
 
 
